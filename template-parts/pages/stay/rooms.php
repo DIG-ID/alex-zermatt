396a616937e77044d00b92fbcf4353829ac45e2c
Daniel-Digid <daniel@dig.id>
2023-09-13 16:57:04 +0100

    d

--- template-parts/pages/stay/rooms.php
+++ template-parts/pages/stay/rooms.php
@@ -3,9 +3,9 @@
         <div class="col-span-1 md:col-span-8 xl:col-span-8 col-start-1 xl:col-start-3">
         <span class="az-vertical-line-img"></span>
         <?php 
-			$intro_image = get_field('section_rooms_image');
-			if( $intro_image ) {
-				echo wp_get_attachment_image( $intro_image, 'full' );
+			$image_rooms = get_field('section_rooms_image');
+			if( $image_rooms ) {
+				echo wp_get_attachment_image( $image_rooms, 'full' );
 			}
 		?>
         </div>

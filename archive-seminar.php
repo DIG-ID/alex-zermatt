7eab51825489ef0655b2ad38549291ec8da282d1
Daniel-Digid <daniel@dig.id>
2023-09-26 14:00:18 +0100

    d

--- archive-seminar.php
+++ archive-seminar.php
@@ -2,8 +2,8 @@
 
 get_header();
 do_action( 'before_main_content' );
-	get_template_part( 'template-parts/pages/stay/seminare/intro' );
-	get_template_part( 'template-parts/pages/stay/seminare/seminare' );
-	get_template_part( 'template-parts/pages/stay/seminare/info' );
+	get_template_part( 'template-parts/archives/seminare/intro' );
+	get_template_part( 'template-parts/archives/seminare/seminare' );
+	get_template_part( 'template-parts/archives/seminare/info' );
 do_action( 'after_main_content' );
 get_footer();

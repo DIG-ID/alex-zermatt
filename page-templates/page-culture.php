c55e78c1a90c0df1a8b4067da900347c8c1ef4a9
Daniel-Digid <daniel@dig.id>
2023-10-06 16:10:45 +0100

    d

--- page-templates/page-culture.php
+++ page-templates/page-culture.php
@@ -10,6 +10,9 @@ if ( have_posts() ) :
 		do_action( 'before_main_content' );
 			do_action( 'before_post_content' );
 				get_template_part( 'template-parts/pages/culture/intro' );
+				get_template_part( 'template-parts/pages/culture/museum' );
+				get_template_part( 'template-parts/pages/culture/zermatt' );
+				get_template_part( 'template-parts/modules/teaser-box-double', null, array( 'paddings' => 'pt-24 xl:pt-44 pb-44 md:pb-20 xl:pb-56', ) );
 			do_action( 'after_post_content' );
 		do_action( 'after_main_content' );
 	endwhile;

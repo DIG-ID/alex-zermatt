6015560c2e9fade192a6b05d70306cb5f27bf4e9
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-10-19 18:36:23 +0100

    up

--- template-parts/pages/experience/activities.php
+++ template-parts/pages/experience/activities.php
@@ -3,14 +3,14 @@
 		<div class="col-span-1 md:col-span-8 xl:col-span-3 xl:col-start-3 order-2 xl:order-1">
 			<div class="grid grid-cols-1 md:grid-cols-8 xl:grid-cols-3 md:gap-4 xl:gap-9">
 				<div class="col-span-1 md:col-span-4 xl:col-span-3">
-					<h3 class="title-xl mb-6 md:mb-0 xl:mb-16 xl:mt-48"><?php the_field( 'sport_title' ); ?></h3>
+					<h3 class="title-xl mb-6 md:mb-0 xl:mb-16 xl:mt-48"><?php the_field( 'activities_title' ); ?></h3>
 				</div>
 				<div class="col-span-1 md:col-span-4 xl:col-span-3">
-					<p class="body-text mb-12 md:mb-14 xl:mb-16"><?php the_field( 'sport_description' ); ?></p>
+					<p class="body-text mb-12 md:mb-14 xl:mb-16"><?php the_field( 'activities_description' ); ?></p>
 				</div>
 			</div>
 			<?php
-			$link = get_field( 'sport_link' );
+			$link = get_field( 'activities_link' );
 			if ( $link ) :
 				$link_url    = $link['url'];
 				$link_title  = $link['title'];
@@ -24,9 +24,9 @@
 			?>
 		</div>
 		<div class="col-span-1 md:col-span-8 xl:col-span-4 xl:col-start-8 order-1 xl:order-2">
-			<h2 class="title-overline !block mx-auto"><?php the_field( 'sport_subtitle' ); ?></h2>
+			<h2 class="title-overline !block mx-auto"><?php the_field( 'activities_subtitle' ); ?></h2>
 			<?php
-			$img = get_field( 'sport_image' );
+			$img = get_field( 'activities_image' );
 			if ( $img ) :
 				echo wp_get_attachment_image( $img, 'full', false, array( 'class' => 'w-full object-cover mb-12 xl:mb-0 max-h-[377px] md:max-h-[800px]' ) );
 			endif;

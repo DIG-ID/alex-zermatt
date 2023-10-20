ed9445a45f24bcdc4a7b1a6958fcbe2af612e55b
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-10-20 17:45:26 +0100

    up

--- archive-sport.php
+++ archive-sport.php
@@ -1,10 +1,10 @@
 <?php
 get_header();
 	do_action( 'before_main_content' );
-		get_template_part( 'template-parts/pages/experience/sport/intro' );
-		get_template_part( 'template-parts/pages/experience/sport/fitness' );
-		get_template_part( 'template-parts/pages/experience/sport/tennis' );
-		get_template_part( 'template-parts/pages/experience/sport/squash' );
-		get_template_part( 'template-parts/pages/experience/sport/golf' );
+		get_template_part( 'template-parts/archives/sport/intro' );
+		get_template_part( 'template-parts/archives/sport/fitness' );
+		get_template_part( 'template-parts/archives/sport/tennis' );
+		get_template_part( 'template-parts/archives/sport/squash' );
+		get_template_part( 'template-parts/archives/sport/golf' );
 	do_action( 'after_main_content' );
 get_footer();

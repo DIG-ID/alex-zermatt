6962e36ab4839a6c6cada9198f93046df2a0af56
Daniel-Digid <daniel@dig.id>
2023-10-25 18:12:51 +0100

    d

--- template-parts/components/card-news.php
+++ template-parts/components/card-news.php
@@ -1,14 +1,14 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'card-news col-span-1 md:col-span-4 xl:col-span-3 mb-24' ); ?>>
     <div class="col-span-1 md:col-span-8 xl:col-span-7 col-start-1 xl:col-start-4 order-2 xl:order-3">
         <?php if (has_post_thumbnail()) : 
-            $full_image_url = wp_get_attachment_image_src(get_post_thumbnail_id(), 'featured-image-museum');
+            $full_image_url = wp_get_attachment_image_src(get_post_thumbnail_id(), 'overall-square');
         ?>
             <div class="featured-image mb-7">
                 <img src="<?php echo $full_image_url[0]; ?>" alt="<?php the_title(); ?>" class="object-cover w-full" />
             </div>
         <?php else : ?>
             <div class="featured-image mb-7">
-                <img src="https://placehold.co/453x453" class="object-cover w-full">
+                <img src="https://placehold.co/453x453" class="object-cover w-full size-overall-square">
             </div>
         <?php endif; 
         $formatted_date = get_the_date('d/m/y');

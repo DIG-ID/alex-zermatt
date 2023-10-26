f09f15b698565a873637bd992dbd38d6043f1da6
Daniel-Digid <daniel@dig.id>
2023-10-26 13:15:01 +0100

    d

--- template-parts/components/tabs-slide.php
+++ template-parts/components/tabs-slide.php
@@ -3,12 +3,12 @@
 		<div class="col-span-1 md:col-span-8 xl:col-span-3">
 			<div class="room-details fade-content">
 				<h3 class="title-xl mb-10 xl:mb-0"><?php the_title(); ?></h3>
-				<p class="text-body !font-bold !text-xl mb-7 mt-9 hidden 2xl:block"><?php the_field( 'overview_subtitle' ); ?></p>
-				<p class="text-body max-w-[450px] hidden 2xl:block"><?php the_field( 'overview_description' ); ?></p>
+				<p class="text-body !font-bold !text-xl mb-7 mt-9 hidden xl:block"><?php the_field( 'overview_subtitle' ); ?></p>
+				<p class="text-body max-w-[450px] hidden xl:block"><?php the_field( 'overview_description' ); ?></p>
 			</div>
 		</div>
 		<div class="col-span-1 md:col-span-8 xl:col-span-7">
-			<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>">
+			<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>" class="h-[200px] md:h-[436px] xl:h-[603px] w-full object-cover wp-post-image">
 		</div>
 		<div class="col-span-1 md:col-span-4 xl:col-span-3 hidden md:block xl:hidden h-[246px]">
 			<p class="text-body !font-bold !text-xl mb-7 mt-9"><?php the_field( 'overview_subtitle' ); ?></p>

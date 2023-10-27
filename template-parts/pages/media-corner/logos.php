cd3cde6829928962c8fda3d23b9d1bee0f836875
Daniel-Digid <daniel@dig.id>
2023-10-27 15:35:57 +0100

    d

--- template-parts/pages/media-corner/logos.php
+++ template-parts/pages/media-corner/logos.php
@@ -6,18 +6,25 @@
     </div>
     <div class="az-container-grid relative z-20">
         <div class="col-span-1 md:col-span-8 xl:col-span-10 col-start-1 xl:col-start-2 grid grid-cols-2 md:grid-cols-8 xl:grid-cols-10 md:gap-4 xl:gap-9">
-        <?php 
-        $images = get_field('galleries_logos');
-        $size = 'full';
-        if( $images ): ?>
-            <?php foreach( $images as $image_id ): ?>
-                <div class="col-span-2 md:col-span-2 xl:col-span-2">
-                    <a class="inline-block w-full mb-6 md:mb-0" data-fancybox="logos-gallery" href="<?php echo wp_get_attachment_image_url($image_id, $size); ?>">
-                        <?php echo wp_get_attachment_image($image_id, 'gallery-square'); ?>
-                    </a>
-                </div>
-            <?php endforeach; ?>
-        <?php endif; ?>
+            <?php 
+            $images = get_field('galleries_logos');
+            $size = 'full';
+            if( $images ): ?>
+                <?php foreach( $images as $image ): ?>
+                    <div class="col-span-2 md:col-span-2 xl:col-span-2">
+                        <a href="<?php echo esc_url($image['url']); ?>" class="inline-block w-full mb-6 md:mb-0" data-fancybox="logos-gallery">
+                            <img src="<?php echo esc_url($image['sizes']['gallery-square']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+                        </a>
+                        <a href="<?php echo esc_url($image['url']); ?>" download="<?php echo $image['filename']; ?>" class="transition-all duration-300 ease-in-out hover:text-gold"><?php  echo $image['filename']; ?></a>
+                        <p class="text-body"><?php  echo $image['width']; esc_html_e( ' x ', 'az' ); echo $image['height']; esc_html_e( ' px', 'az' ); ?></p>
+                        <?php
+                        $filesize = $image['filesize'];
+                        $filesize_in_mb = $filesize / (1024 * 1024);
+                        echo '<p class="text-body">' . number_format($filesize_in_mb, 2) . ' MB</p>';
+                        ?>
+                    </div>
+                <?php endforeach; ?>
+            <?php endif; ?>
         </div>
     </div>
 </section>

f65384a4e45b73d834bae70e5d90677483ad981d
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-10-31 13:03:31 +0000

    up

--- assets/js/gsap.js
+++ assets/js/gsap.js
@@ -80,7 +80,7 @@ $(function() {
       scrollTrigger: {
         trigger: ".section-intro",
       },
-      defaults: { duration: .6 },
+      defaults: { duration: .4 },
     })
     .from('.section-intro img', { ...defaultitemcfg })
     .from('.section-intro .title-xl', { ...defaultitemcfg })

a5509d6ff664ed3ba32622c6551f0b860e410eca
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-10-31 16:28:44 +0000

    ~up

--- template-parts/pages/eat-&-drink/le-jardin/intro.php
+++ template-parts/pages/eat-&-drink/le-jardin/intro.php
@@ -4,13 +4,13 @@
 			<?php
 			$image = get_field( 'intro_image' );
 			if ( $image ) :
-				echo wp_get_attachment_image( $image, 'full', null, array( 'class' => 'h-[380px] md:h-[800px] xl:h-[780px] mb-20 md:mb-12 xl:mb-0 object-cover' ) );
+				echo wp_get_attachment_image( $image, 'full', null, array( 'class' => 'h-[380px] md:h-[800px] xl:h-[780px] mb-20 md:mb-12 xl:mb-0 object-cover invisible' ) );
 			endif;
 			?>
 		</div>
 		<div class="col-span-1 md:col-span-8 xl:col-span-3 xl:col-start-8 text-center xl:text-left">
-			<h1 class="title-xl mb-12 xl:mb-24 md:max-w-xl xl:max-w-none mx-auto"><?php the_field( 'intro_title' ); ?></h1>
-			<p class="text-body mb-24 xl:mb-0 md:max-w-lg xl:max-w-none mx-auto"><?php the_field( 'intro_description' ); ?></p>
+			<h1 class="title-xl mb-12 xl:mb-24 md:max-w-xl xl:max-w-none mx-auto invisible"><?php the_field( 'intro_title' ); ?></h1>
+			<p class="text-body mb-24 xl:mb-0 md:max-w-lg xl:max-w-none mx-auto invisible"><?php the_field( 'intro_description' ); ?></p>
 		</div>
 	</div>
 </section>

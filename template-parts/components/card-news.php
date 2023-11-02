7b1a62a0de434d6450c6aa97cf9c31c0ce948cfe
Daniel-Digid <daniel@dig.id>
2023-11-02 13:31:39 +0000

    d

--- template-parts/components/card-news.php
+++ template-parts/components/card-news.php
@@ -14,7 +14,7 @@
         $formatted_date = get_the_date('d/m/y');
         echo '<p class="title-sub mb-5">' . $formatted_date . '</p>';
         ?>
-        <h2 class="title-xl mb-4"><?php the_title(); ?></h2>
+        <h2 class="font-serif font-extralight text-2xl md:text-[1.75rem] xl:text-[1.25rem] text-blue leading-7 md:leading-none tracking-[1.1px] md:tracking-[1.4px] xl:tracking-[2.6px] uppercase break-words mb-4"><?php the_title(); ?></h2>
         <div class="text-body mb-6 md:mb-7 xl:mb-10 max-w-none md:max-w-[393px]"><?php the_excerpt(); ?></div>
         <span class="block text-right md:text-left mr-16 xl:mr-0"><a class="btn btn--arrow-right" href="<?php the_permalink(); ?>"><?php echo esc_html_e( 'Mehr Erfahren', 'alexzermatt' ) ?></a></span>
     </div>

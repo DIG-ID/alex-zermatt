ffb799e942c9ab9c39474a16707d7c024dbd1dc2
Daniel-Digid <daniel@dig.id>
2023-11-03 11:37:19 +0000

    d

--- template-parts/pages/about/chef.php
+++ template-parts/pages/about/chef.php
@@ -1,5 +1,5 @@
 <section id="section-chef" class="section-chef az-container xl:pb-32 overflow-hidden">
-	<div class="az-container-grid">
+	<div class="az-container-grid mb-14 xl:mb-0">
         <div class="col-span-1 md:col-span-8 xl:col-span-3 col-start-1 xl:col-start-3 flex flex-col justify-center">
     	    <h2 class="title-overline z-20"><?php the_field( 'section_chef_title_overline' ); ?></h2>
             <?php 
@@ -7,25 +7,25 @@
             if ( $image_3 ) : 
             ?>
                 <div class="featured-image z-10">
-                <?php echo wp_get_attachment_image( $image_3, 'full' ); ?>
+                <?php echo wp_get_attachment_image( $image_3, 'angebote-slider-image' ); ?>
                 </div>
             <?php else : ?>
                 <div class="featured-image z-10">
                     <img src="https://placehold.co/453x462">
                 </div>
             <?php endif; ?>
-            <h3 class="title-xl mt-24 mb-20 xl:mb-28"><?php the_field( 'section_chef_chef_title' ); ?></h3>
+            <h3 class="title-xl mt-6 mb-4 md:mt-11 xl:mt-24 xl:mb-28"><?php the_field( 'section_chef_chef_title' ); ?></h3>
             <p class="text-body"><?php the_field( 'section_chef_chef_description' ); ?></p>
         </div>
     </div>
-    <div class="az-container-grid">
+    <div class="az-container-grid mb-14 xl:mb-0">
         <div class="col-span-1 md:col-span-8 xl:col-span-4 col-start-1 xl:col-start-7 flex flex-col justify-center xl:-mt-[34rem]">
             <?php 
             $image_4 = get_field('section_chef_image_2');
             if ( $image_4 ) : 
             ?>
                 <div class="featured-image">
-                <?php echo wp_get_attachment_image( $image_4, 'full' ); ?>
+                <?php echo wp_get_attachment_image( $image_4, 'main-header-image-half-container' ); ?>
                 </div>
             <?php else : ?>
                 <div class="featured-image">

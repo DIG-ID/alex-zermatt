a942b1be8af4d30dd0562cb15f64d8a3733dff62
Daniel-Digid <daniel@dig.id>
2023-11-03 15:44:19 +0000

    d

--- template-parts/pages/home/links.php
+++ template-parts/pages/home/links.php
@@ -1,4 +1,4 @@
-<section class="section-links az-container relative">
+<section class="section-links az-container relative pb-16 xl:pb-0">
 	<div class="az-container-grid">
 		<div class="col-span-1 md:col-span-8 xl:col-span-8 xl:col-start-3 flex justify-between py-10">
 			<?php
@@ -8,7 +8,7 @@
 				$link_title  = $tr_link['title'];
 				$link_target = $tr_link['target'] ? $tr_link['target'] : '_self';
 				?>
-				<a class="btn" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>" data-fancybox="reservation-home" data-caption="<?php echo esc_html( $link_title ); ?>"><?php echo esc_html( $link_title ); ?></a>
+				<a class="btn absolute md:relative inline-block md:block -top-40 md:top-auto left-1/2 md:left-auto -translate-x-1/2 md:translate-x-0" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>" data-fancybox="reservation-home" data-caption="<?php echo esc_html( $link_title ); ?>"><?php echo esc_html( $link_title ); ?></a>
 				<?php
 			endif;
 			$br_link = get_field( 'booking_links_booking_reservation', 'options' );
@@ -17,7 +17,7 @@
 				$link_title  = $br_link['title'];
 				$link_target = $br_link['target'] ? $br_link['target'] : '_self';
 				?>
-				<a class="btn btn-overline !font-medium absolute inline-block -top-52 left-1/2 -translate-x-1/2" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+				<a class="btn btn-overline-home !font-medium absolute inline-block -top-52 left-1/2 -translate-x-1/2" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
 				<?php
 			endif;
 			$vouchers_link = get_field( 'booking_links_vouchers', 'options' );
@@ -26,7 +26,7 @@
 				$link_title  = $vouchers_link['title'];
 				$link_target = $vouchers_link['target'] ? $vouchers_link['target'] : '_self';
 				?>
-				<a class="btn" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+				<a class="btn absolute md:relative inline-block md:block -top-28 md:top-auto left-1/2 md:left-auto -translate-x-1/2 md:translate-x-0" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
 				<?php
 			endif;
 			?>

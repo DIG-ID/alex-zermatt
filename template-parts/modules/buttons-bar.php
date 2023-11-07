e4ba087a99b2a5df5ebcc7fd9cf4c0bfb7d9d3e9
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-11-07 12:20:05 +0000

    up

--- template-parts/modules/buttons-bar.php
+++ template-parts/modules/buttons-bar.php
@@ -1,4 +1,4 @@
-<section class="section-buttons-bar fixed left-0 right-0 bottom-0 z-10 bg-white drop-shadow-[0_-2px_2px_rgba(0,0,0,0.06)]">
+<section class="section-buttons-bar fixed left-0 right-0 bottom-0 z-30 bg-white drop-shadow-[0_-2px_2px_rgba(0,0,0,0.06)]">
 	<div class="az-container-grid az-container">
 		<div class="col-span-1 md:col-span-8 xl:col-span-8 xl:col-start-3 flex sm:justify-between justify-center items-center py-4 sm:py-10">
 			<?php

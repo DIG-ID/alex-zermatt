63225e05347bd46f75831109c7f6362dea19faa7
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-11-09 19:06:31 +0000

    up

--- template-parts/pages/stay/information/intro.php
+++ template-parts/pages/stay/information/intro.php
@@ -8,7 +8,7 @@
             <?php 
                 $intro_image = get_field( 'section_intro_image' );
                 if( $intro_image ) {
-                    echo wp_get_attachment_image( $intro_image, 'main-header-image-half-container', false, array( 'class' => 'invisible' ) );
+                    echo wp_get_attachment_image( $intro_image, 'main-header-image-half-container', false, array( 'class' => 'h-[380px] md:h-[800px] xl:h-[780px] mb-20 md:mb-12 xl:mb-0 object-cover invisible' ) );
                 }
             ?>
         </div>

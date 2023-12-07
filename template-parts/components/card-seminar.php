2a79b522b2eac22ea84bd52c3d1c3b46fd9c4e61
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-12-07 11:14:28 +0000

    up

--- template-parts/components/card-seminar.php
+++ template-parts/components/card-seminar.php
@@ -35,11 +35,11 @@
 						$image_alt    = strtolower($type[0]);
 
 						echo '<div class="col-span-1 text-center text-body !text-[0.75rem] !break-keep">';
-						echo '<div class="text-body !text-[.75rem]">' . $type[2] . '</div>';
 
 						$capacity_value = get_field( $capacity_key );
 
 						if ( $capacity_value ) {
+							echo '<div class="text-body !text-[.75rem]">' . $type[2] . '</div>';
 							echo "<img class=\"mx-auto my-4\" src=\"" . get_stylesheet_directory_uri() . $image_path . "\" title=\"$image_alt\" alt=\"$image_alt\">";
 							echo "<p class=\"text-body\">" . get_field($capacity_key) . "</p>";
 						}
@@ -75,15 +75,15 @@
 				$image_alt    = strtolower($type[0]);
 
 				echo '<div class="col-span-1 text-center text-body !text-[0.75rem] !break-keep">';
-				echo '<div class="text-body">' . $type[2] . '</div>';
+				
 
 				$capacity_value = get_field( $capacity_key );
 
 				if ( $capacity_value ) {
+					echo '<div class="text-body">' . $type[2] . '</div>';
 					echo "<img class=\"mx-auto my-4\" src=\"" . get_stylesheet_directory_uri() . $image_path . "\" title=\"$image_alt\" alt=\"$image_alt\">";
 					echo "<p class=\"text-body\">" . get_field($capacity_key) . "</p>";
 				}
-
 				echo '</div>';
 			}
 			?>

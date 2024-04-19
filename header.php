cc88d5a5c71ccf0db3ee20d0434db854e59ae1ae
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2024-04-19 17:50:39 +0100

    up

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
 		<!-- Start cookieyes banner -->
-		<script>
+		<!--<script>
 		window.dataLayer = window.dataLayer || [];
 		function gtag() {
 				dataLayer.push(arguments);
@@ -21,7 +21,7 @@
 		});
 		gtag("set", "ads_data_redaction", true);
 		gtag("set", "url_passthrough", true);
-		</script>
+		</script>-->
 		<!-- Google Tag Manager -->
 		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -29,7 +29,7 @@
 		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 		})(window,document,'script','dataLayer','GTM-TPWSW4LW');</script>
 		<!-- End Google Tag Manager -->
-		<script id="cookieyes" type="text/javascript" src="https://cdn-cookieyes.com/client_data/877c1d9f3ca6a5e718cf89c9/script.js"></script>
+		<!--<script id="cookieyes" type="text/javascript" src="https://cdn-cookieyes.com/client_data/877c1d9f3ca6a5e718cf89c9/script.js"></script>-->
 		<!-- End cookieyes banner -->
 		<?php wp_head(); ?>
 	</head>

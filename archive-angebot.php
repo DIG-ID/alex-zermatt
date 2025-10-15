e9b36b5e499631efd238f012199c933c1e8cfcfe
Daniel-Digid <daniel@dig.id>
2025-10-15 13:07:02 +0100

    Update archive-angebot.php

--- archive-angebot.php
+++ archive-angebot.php
@@ -3,7 +3,7 @@
 get_header();
 do_action( 'before_main_content' );
 	get_template_part( 'template-parts/archives/angebot/intro' ); 
-	get_template_part( 'template-parts/archives/angebot/summer-slider' );
 	get_template_part( 'template-parts/archives/angebot/winter-slider' );
+	get_template_part( 'template-parts/archives/angebot/summer-slider' );
 do_action( 'after_main_content' );
 get_footer();

3f49895bf2d6f67d01965e2be9f43662b67bda73
bankz91 <brunoduarte.91300@gmail.com>
2026-03-19 16:28:04 +0000

    1

--- template-parts/archives/spielelounge/intro.php
+++ template-parts/archives/spielelounge/intro.php
@@ -4,13 +4,13 @@
 			<?php
 			$image = get_field( 'archive_spielelounge_intro_image', 'options' );
 			if ( $image ) :
-				echo wp_get_attachment_image( $image, 'full', null, array( 'class' => 'h-[380px] md:h-[800px] xl:h-[780px] mb-20 md:mb-12 xl:mb-0 object-cover invisible' ) );
+				echo wp_get_attachment_image( $image, 'full', null, array( 'class' => 'h-[380px] md:h-[800px] xl:h-[780px] mb-20 md:mb-12 xl:mb-0 object-cover' ) );
 			endif;
 			?>
 		</div>
 		<div class="col-span-1 md:col-span-8 xl:col-span-3 xl:col-start-8 text-center xl:text-left">
-			<h1 class="title-xl !hyphens-none mb-12 xl:mb-24 md:max-w-xl xl:max-w-none mx-auto invisible"><?php the_field( 'archive_spielelounge_intro_title', 'options' ); ?></h1>
-			<p class="text-body mb-24 xl:mb-0 md:max-w-lg xl:max-w-none mx-auto invisible"><?php the_field( 'archive_spielelounge_intro_description', 'options' ); ?></p>
+			<h1 class="title-xl !hyphens-none mb-12 xl:mb-24 md:max-w-xl xl:max-w-none mx-auto"><?php the_field( 'archive_spielelounge_intro_title', 'options' ); ?></h1>
+			<p class="text-body mb-24 xl:mb-0 md:max-w-lg xl:max-w-none mx-auto"><?php the_field( 'archive_spielelounge_intro_description', 'options' ); ?></p>
 		</div>
 	</div>
 </section>

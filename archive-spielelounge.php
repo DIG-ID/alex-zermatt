85deb284007d6ec2599f4ce0f21714ca34de220a
bankz91 <brunoduarte.91300@gmail.com>
2026-03-20 14:47:51 +0000

    11

--- archive-spielelounge.php
+++ archive-spielelounge.php
@@ -2,6 +2,6 @@
 get_header();
 do_action( 'before_main_content' );
 	get_template_part( 'template-parts/archives/spielelounge/intro' );
-	get_template_part( 'template-parts/archives/spielelounge/list' );
+	get_template_part( 'template-parts/archives/spielelounge/posts' );
 do_action( 'after_main_content' );
 get_footer();

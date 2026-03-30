db8046644a68ceb688a5f06d01c71658a95cb738
bankz91 <brunoduarte.91300@gmail.com>
2026-03-30 11:33:54 +0100

    1

--- archive-activities.php
+++ archive-activities.php
@@ -2,7 +2,7 @@
 get_header();
 	do_action( 'before_main_content' );
 		get_template_part( 'template-parts/archives/activities/intro' );
-		get_template_part( 'template-parts/archives/activities/winter' );
 		get_template_part( 'template-parts/archives/activities/sommer' );
+		get_template_part( 'template-parts/archives/activities/winter' );
 	do_action( 'after_main_content' );
 get_footer();
